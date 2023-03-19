fbb31a3db4df8325d12fdb726a592150692e08da
WazedKhan <wazedkhan119399@gmail.com>
2023-03-19 23:48:14 +0600

    adding functional validator to add custom condition

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from .models import Movie
 
 
+def is_rating(value):
+    if value < 1:
+        raise serializers.ValidationError("Value cannot be lower than 1")
+    elif value > 10:
+        raise serializers.ValidationError("Value cannot be higher than 10")
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(validators=[is_rating])
     class Meta:
         model = Movie
         fields = "__all__"
@@ -14,5 +22,7 @@ class MovieSerializer(serializers.ModelSerializer):
 
         def validate(self, data):
             if data["us_gross"] > data["worldwide_gross"]:
-                raise serializers.ValidationError("us_gross can't be bigger then worldwide gross")
+                raise serializers.ValidationError(
+                    "us_gross can't be bigger then worldwide gross"
+                )
             return data
