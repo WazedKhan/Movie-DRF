fda59547fa4dc1efede782ad8374c60cf80b06b5
WazedKhan <wazedkhan119399@gmail.com>
2023-03-19 23:11:24 +0600

    custom field validation

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,4 +5,9 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = "__all__"
+
+        def validate_rating(self, value):
+            if value < 1 and value > 10:
+                raise serializers.ValidationError("Rating has to be between 1 to 10")
+            return value
