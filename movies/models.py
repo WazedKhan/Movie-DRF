9355e24b2ea45014fef2f5a11b4ee9ff8f0caa06
WazedKhan <wazedkhan119399@gmail.com>
2023-03-19 21:34:03 +0600

    model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048)
+    release_date = models.DateField()
+    rating = models.PositiveSmallIntegerField()
+
+    us_gross = models.IntegerField(default=0)
+    worldwide_gross = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title}'
